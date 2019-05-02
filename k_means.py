bd2aff3b3fbfbb74e10ebcd3f91cb797916beec8
nirpa <nirparai@yahoo.com>
2019-05-02 09:39:28 +0545

    k means finalize

--- k_means.py
+++ k_means.py
@@ -76,25 +76,6 @@ class K_Means:
 				plt.text(cordinates[0], cordinates[1], 'predicted cluster')
 		plt.show()
 
-# data_set = pd.read_excel('titanic.xls')
-# # K_Means(data_set)
-# data_set.drop(['name'], 1, inplace=True)
-# data_set.fillna(0, inplace=True)
-# columns = data_set.columns.values
-# for column in columns:
-#     if data_set[column].dtype != np.int64 and data_set[column].dtype != np.float64:
-#         data_set[column] = data_set[column].astype('category')
-#         data_set[column] = data_set[column].cat.codes
-# print(data_set.head())
-
-# # X = data_set.drop(['survived'], 1, inplace=True)
-# # X = np.array(X)
-# # X= X.astype(float)
-
-# X = np.array(data_set.drop(['survived'], 1).astype(float))
-# X = preprocessing.scale(X)
-
-
 X = np.array([
 			[1, 2],
 			[5, 8],
