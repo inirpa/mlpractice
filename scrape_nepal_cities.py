eab4914c187cb75f019a371fd202f2856411974a
nirpa <nirparai@yahoo.com>
2019-05-14 11:17:07 +0545

    header included

--- scrape_nepal_cities.py
+++ scrape_nepal_cities.py
@@ -6,7 +6,7 @@ source = urllib.request.urlopen('https://en.wikipedia.org/wiki/List_of_cities_in
 soup = bs.BeautifulSoup(source, 'lxml')
 tables = soup.find_all('table',{'class':'wikitable sortable'})
 
-rows = [['Rank', 'Name', 'Nepali', 'District', 'Province', 'Population (2011)', 'Area', 'Website']]
+rows = []
 for table in tables:
 	table_rows = table.find_all('tr')
 	for tr in table_rows:
@@ -15,6 +15,7 @@ for table in tables:
 		for i in td:
 			cols.append(i.text)
 		rows.append(cols)
-
+index = ['Rank', 'Name', 'Nepali', 'District', 'Province', 'Population (2011)', 'Area', 'Website'];
 df = pd.DataFrame(rows)
+df.columns = index
 df.to_excel('cities_of_nepal.xls')
