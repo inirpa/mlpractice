e66551be1be55ea06a49333aa0fb57fa90eae86a
nirpa <nirparai@yahoo.com>
2019-05-14 10:28:10 +0545

    cities of nepal scrapped from wiki

--- scrape_nepal_cities.py
+++ scrape_nepal_cities.py
@@ -1,23 +1,20 @@
 import bs4 as bs
 import urllib.request
 import pandas as pd
-from IPython.display import display_html
 
 source = urllib.request.urlopen('https://en.wikipedia.org/wiki/List_of_cities_in_Nepal')
 soup = bs.BeautifulSoup(source, 'lxml')
-tables = soup.find('table',{'class':'wikitable sortable'})
+tables = soup.find_all('table',{'class':'wikitable sortable'})
 
-# print(tables)
-# display_html(tables, raw=True)
-# for table in tables:
-dfs = pd.read_html(tables)
-print(dfs)
-# df = pd.DataFrame()
-# for table in tables:
-#     table_rows = table.find_all('tr')
-#     for tr in table_rows:
-#         td = tr.find_all('td')
-#         for i in td:
-#             # print(i.text)
-#             df = pd.DataFrame([i.text])
-#         df.to_excel('new.xls')
+rows = [['Rank', 'Name', 'Nepali', 'District', 'Province', 'Population (2011)', 'Area', 'Website']]
+for table in tables:
+	table_rows = table.find_all('tr')
+	for tr in table_rows:
+		td = tr.find_all('td')
+		cols = []
+		for i in td:
+			cols.append(i.text)
+		rows.append(cols)
+
+df = pd.DataFrame(rows)
+df.to_excel('cities_of_nepal.xls')
